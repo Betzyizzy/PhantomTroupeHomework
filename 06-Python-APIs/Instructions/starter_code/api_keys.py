ec3551002a314ecfe128b9d0e49ef418bbdd82ce
betzyizzy <bethzaidaguerra@gmail.com>
2021-09-06 16:39:12 -0500

    addd Python API homework

--- 06-Python-APIs/Instructions/starter_code/api_keys.py
+++ 06-Python-APIs/Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "58b9fb014aca3ca0cf6747573ec8fa2f"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBqFc-D6FCL1Joj3RLc6FRhgV6cCKvlq9o"
